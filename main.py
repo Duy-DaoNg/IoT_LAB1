09bc435a1731469c61f8504835c3c3e240a73039
Duy-DaoNg <duy.dao0604@gmail.com>
2024-01-22 11:05:53 +0700

    inital commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+
+AIO_FEED_ID = ["nutnhan1", "nutnhan2"]
+AIO_USERNAME = "duydao0604"
+AIO_KEY = "aio_rTSc5759iet2mJUIS53su7F5AIXy"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_ID:
+        client.subscribe(topic)
+
+def subscribe(client, userdata, mid, granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit(1)
+
+def message(client, feed_id, payload):
+    print("Nhan du lieu: " + payload)
+
+client = MQTTClient(AIO_USERNAME, AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+sensor_type = 0
+while True:
+    counter = counter - 1
+    if counter <= 0:
+        counter = 10
+        #TODO
+        if sensor_type == 0:
+            print("Temparature...")
+            sensor_type = 1
+            temp = random.randint(15, 45)
+            client.publish("cambien1", temp)
+        elif sensor_type == 1:
+            print("Humidity...")
+            sensor_type = 2
+            light = random.randint(100, 500)
+            client.publish("cambien2", light)
+        else:
+            print("Light...")
+            sensor_type = 0
+            humi = random.randint(50, 70)
+            client.publish("cambien3", humi)
+    time.sleep(1)
+
+    pass
